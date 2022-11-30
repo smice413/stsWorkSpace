ee1b63fab19abb15180097c75da48c290291c0c4
smice413 <sungho413@naver.com>
2022-11-30 22:27:03 +0900

    springtest 커밋

---
+++ springtest/src/main/java/com/myhome/springtest/HomeController.java
@@ -0,0 +1,51 @@
+package com.myhome.springtest;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	/* @RequestMapping 값을 받는 역할 "/"는 모든 요청을 받는다는 의미. *.do로 해도된다. 
+	  또한 model은 request객체와 같은 역할 */
+	@RequestMapping(value = "/first", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping("second")
+	public String second(Model model) {
+		
+		model.addAttribute("test", "값전달 성공");
+		
+		return "result";
+	
+	}
+	
+	
+}
---
+++ springtest/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+
+<P>  The time on the server is ${serverTime}. </P>
+<br><br>
+
+<a href="second">이동</a>
+
+</body>
+</html>
